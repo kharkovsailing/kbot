d77d2bc3866bd5000745bea210da0071542b96d5
Olga Kostina <kharkovsailing@users.noreply.github.com>
2024-03-27 07:40:37 +0000

    version v1.0.2 updated

--- cmd/version.go
+++ cmd/version.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 */
 package cmd
 
@@ -14,14 +14,14 @@ var appVersion = "Version"
 
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
-	Use:   "Version",
+	Use:   "version",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
-	and usage of using your command. For example:
+and usage of using your command. For example:
 
-	Cobra is a CLI library for Go that empowers applications.
-	This application is a tool to generate the needed files
-	to quickly create a Cobra application.`,
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println(appVersion)
 	},
